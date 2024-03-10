44445dbb8f37a2713f34808a0129581420054b63
eharris733 <elliotmharris@gmail.com>
2024-03-09 21:31:27 -0500

    created a new dataset with only winning side positions
    Will need to get a bigger dataset, as this is only half or less the desire size

--- dbs/mastergames/extractor.py
+++ dbs/mastergames/extractor.py
@@ -8,22 +8,35 @@ def extract_random_positions_from_pgn(file_path, output_csv):
         writer.writerow(['FEN', 'Move'])  # Header for the CSV file
 
         while True:
+
             game = chess.pgn.read_game(pgn)
             if game is None:
                 break  # End of file reached
+
+            result = game.headers["Result"]
+
+            if result == "1-0":
+                winningSide = True
+            elif result == "0-1":
+                winningSide = False
+            else:
+                continue
+
+
 
             board = game.board()
             positions = []  # Correctly initialize an empty list for positions
             mc = 0
             for move in game.mainline_moves():
-                fen = board.fen()
-                board.push(move)
-                mc += 1
-                if mc > 10:
-                    # Only consider positions after the 10th move to avoid opening positions
-                    # and make sure the game hasn't reached an end state with too few pieces
-                    if len(board.piece_map()) > 6:
-                        positions.append((fen, move.uci()))
+                if game.turn() == winningSide: # only take from sides that eventually won
+                    fen = board.fen()
+                    board.push(move)
+                    mc += 1
+                    if mc > 10:
+                        # Only consider positions after the 10th move to avoid opening positions
+                        # and make sure the game hasn't reached an end state with too few pieces
+                        if len(board.piece_map()) > 6:
+                            positions.append((fen, move.uci()))
 
             # Check if positions list is not empty before choosing a random position
             if positions:
